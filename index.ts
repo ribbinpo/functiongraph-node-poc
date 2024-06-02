b90a6fa990e88a8250fc741bb5c4b2c36e40d1c7
ribbinpo <ribbinpo@gmail.com>
2024-06-02 20:04:31 +0700

    feat: :sparkles: add global module running

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import templateRouter from "./functions/templates/src/routes/index";
+
+import { errorHandler } from "./shares/middlewares/error.middleware";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/template", templateRouter);
+
+app.get("/health-check", (req, res) => {
+  res.status(200).send("Server is running :)");
+});
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
