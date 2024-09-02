9895965a859369a09324d9aa3541fbcbfb3b6d21
ribbinpo <ribbinpo@gmail.com>
2024-09-02 15:57:32 +0700

    feat: :sparkles: add response format and error middleware

---
+++ shares/utils/response/index.ts
@@ -0,0 +1,17 @@
+import {
+  ErrorHandler,
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+} from "./error.util";
+import { SuccessHandler } from "./success.util";
+
+export {
+  ErrorHandler,
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+  SuccessHandler,
+};
